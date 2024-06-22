c2d8e9ecea4e35e5b0f18e8c19229b99e02bca12
TsanthoshR <santhosht5290@gmail.com>
2024-06-23 00:18:39 +0530

    fix startgame and newcard buttons

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let firstCard = 10
-let secondCard = 4
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -8,14 +9,36 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 console.log(messageEl)
 
-function startGame() {
+// let sumEl = document.getElementById("sum-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
+
+let player = {
+    name: "Player1",
+    chips: 4000
+}
+
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+function renderGame() {
+
+    sumEl.textContent = "Score: " + sum
+    // cardsEl.textContent = "Cards: " + firstCard + " " + secondCard + " "
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card?" //🙂"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "Wohoo! You've got Blackjack!" //🥳"
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"// 😭"
         isAlive = false
     }
 
@@ -24,4 +47,45 @@ function startGame() {
     console.log("clicked")
     // window.alert("clicked")
     console.log(message)
+}
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+
+    renderGame()
+}
+
+function newCard() {
+    if (isAlive && sum <21){
+
+        console.log("Drawing a new card ")
+
+        let nextCard = getRandomCard();
+
+        cards.push(nextCard)
+        console.log(nextCard)
+
+        sum += nextCard
+
+        renderGame()
+    }
+
+}
+
+function getRandomCard() {
+
+    let card = Math.floor( Math.random()*13 ) + 1
+    if (card === 1) {
+        return 11
+    } else if (card >10) {
+        return 10
+    } else {
+        return card
+    }
+    // return 5;
+
 }
